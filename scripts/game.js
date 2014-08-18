d5fe97ccdcd8bab3dad5c7a7c56491a53c1c5e4f
Mark Vasilkov <mvasilkov@gmail.com>
2014-08-18 19:43:58 +0300

    Changes

--- scripts/game.js
+++ scripts/game.js
@@ -1,22 +1,22 @@
 var a = new Moon(0, 0, 50, NEUTRAL)
 a.render()
 
-var r = 250
+var r = 250, skew = -0.05 * Math.PI
 
 var b = new Moon(
-    r * Math.cos(0),
-    r * Math.sin(0),
+    r * Math.cos(skew),
+    r * Math.sin(skew),
     50, FIRE)
 b.render()
 
 var c = new Moon(
-    r * Math.cos(4 * Math.PI / 3)|0,
-    r * Math.sin(4 * Math.PI / 3)|0,
+    r * Math.cos(4 * Math.PI / 3 + skew)|0,
+    r * Math.sin(4 * Math.PI / 3 + skew)|0,
     50, WATER)
 c.render()
 
 var d = new Moon(
-    r * Math.cos(2 * Math.PI / 3)|0,
-    r * Math.sin(2 * Math.PI / 3)|0,
+    r * Math.cos(2 * Math.PI / 3 + skew)|0,
+    r * Math.sin(2 * Math.PI / 3 + skew)|0,
     50, EARTH)
 d.render()
--- scripts/util.js
+++ scripts/util.js
@@ -4,8 +4,11 @@ function rand0(a) { return a * (Math.random() - 0.5) }
 var L1 = $id('L1'), L2 = $id('L2')
 var R1 = L1.getContext('2d'), R2 = L2.getContext('2d')
 
-R1.translate(0.5 * 900 + 0.5, 0.5 * 600 + 0.5)
-R2.translate(0.5 * 900 + 0.5, 0.5 * 600 + 0.5)
+var x0 = 0.5 * 900 + 0.5 - 40
+var y0 = 0.5 * 600 + 0.5 - 20
+
+R1.translate(x0, y0)
+R2.translate(x0, y0)
 
 var NEUTRAL = 0, FIRE = 1, WATER = 2, EARTH = 3
 
